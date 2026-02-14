260703f32b1f7600ae97e536be4da096e3bb5326
JeremyT21 <thummeljeremy@gmail.com>
2026-02-13 19:19:51 -0500

    Added the program that you continuously type in terminal to, so you can build a file of typing log data.

---
+++ final_draft/get_user_log_data.py
@@ -0,0 +1,60 @@
+import csv
+import os
+import random
+import time
+
+#need to install random_word library (use pip install random_word)
+from random_word import RandomWords
+
+csv_path = "typing_log.csv"
+user_id = "u1"
+
+header = ["user_id", "word", "time_ms", "mistypes"]
+
+def generate_word_list(n):
+    r = RandomWords()
+    words = []
+
+    while len(words) < n:
+        words.append(r.get_random_word().lower())
+
+    return words
+    
+words_to_type = generate_word_list(520)
+
+#adding rows to the csv of inputted data
+def append_row(path, user_id, word, time_ms, mistypes):
+    with open(path, "a", newline="", encoding="utf-8") as writing_to_csv:
+        csv.writer(writing_to_csv).writerow([user_id, word, time_ms, mistypes])
+
+#format words
+def reformatted(word):
+    return word.strip().lower()
+
+def get_word_type_data():
+    while True:
+        target = random.choice(words_to_type)
+        target_norm = reformatted(target)
+
+        print(f"please type word: {target}")
+        start = time.perf_counter()
+        wrong_enters = 0
+
+        while True:
+            typed = input("TYPE: ")
+            if reformatted(typed) == "quit":
+                print("all words typed")
+                return
+
+            if reformatted(typed) == target_norm:
+                #gets time until enter pressed after typing word in terminal
+                elapsed_ms = int((time.perf_counter() - start) * 1000)
+                append_row(csv_path, user_id, target, elapsed_ms, wrong_enters)
+                print(f"RIGHT ANSWER - ADDED TO CSV: {user_id},{target},{elapsed_ms},{wrong_enters}\n")
+                break
+            else:
+                wrong_enters += 1
+                print("WRONG - RETYPE")
+
+#run the logging loop
+get_word_type_data()
